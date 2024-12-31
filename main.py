6d347ae8673a32d8c3908588261026bdfd393365
Сергей Прокопенко <psi200523@gmail.com>
2025-01-01 04:04:46 +0900

    add discord pofile

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def get_root():
-    with open("static/index.html", "r", encoding="utf-8") as file:  # Указываем кодировку при чтении файла
+    with open("static/index.html", "r", encoding="utf-8") as file:
         content = file.read()
-    return HTMLResponse(content=content, headers={"Content-Type": "text/html; charset=utf-8"})  # Указываем кодировку в заголовке ответа
+    return HTMLResponse(content=content, headers={"Content-Type": "text/html; charset=utf-8"})

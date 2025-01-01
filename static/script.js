463f6b14368118cb80b1e331d01a027316d9b9a6
Сергей Прокопенко <psi200523@gmail.com>
2025-01-02 07:34:15 +0900

    OOP_script

--- static/script.js
+++ static/script.js
@@ -1,21 +1,69 @@
-// Элемент для вывода текста
-const output = document.getElementById("output");
+class TextWriter {
+    constructor(element, speed = 50) {
+        this.element = element;
+        this.speed = speed;
+    }
 
-// Элемент для вывода меню
-const outputArea = document.getElementById("output-area");
+    typeText(text, callback) {
+        let i = 0;
+        const interval = setInterval(() => {
+            if (i < text.length) {
+                this.element.textContent += text[i];
+                i++;
+            } else {
+                clearInterval(interval);
+                if (callback) callback();
+            }
+        }, this.speed);
+    }
+}
 
-// Элемент для вывода текста после меню
-const outputBottom = document.getElementById("bottom-output");
+class MenuRenderer {
+    constructor(outputArea, menuItems, speed = 50) {
+        this.outputArea = outputArea;
+        this.menuItems = menuItems;
+        this.speed = speed;
+    }
 
-// Текст, который выводится первым
+    typeMenuItem(menuItem, callback) {
+        let i = 0;
+
+        // Создаём элемент ссылки
+        const menuLink = document.createElement("a");
+        menuLink.href = menuItem.link;
+        menuLink.target = "_blank";
+        menuLink.className = "menu-item";
+
+        this.outputArea.appendChild(menuLink);
+
+        const interval = setInterval(() => {
+            if (i < menuItem.text.length) {
+                menuLink.textContent += menuItem.text[i];
+                i++;
+            } else {
+                clearInterval(interval);
+                if (callback) callback();
+            }
+        }, this.speed);
+    }
+
+    renderMenu(callback) {
+        const renderMenuRecursively = (index) => {
+            if (index < this.menuItems.length) {
+                this.typeMenuItem(this.menuItems[index], () => renderMenuRecursively(index + 1));
+            } else if (callback) {
+                callback();
+            }
+        };
+        renderMenuRecursively(0);
+    }
+}
+
+// Использование классов
 const introText = "Добро пожаловать на мой первый сайт.\n\nМеня зовут Прокопенко Сергей Игоревич. Я студент Дальневосточного Федерального Университета.";
-
-// Текст, который выводится после меню
 const bottomText = "Все права защищены© ";
-
-// Пункты меню
 const menuItems = [
-    { text: "1.VK", link: "https://vk.com/oo0010090", icon: "https://upload.wikimedia.org/wikipedia/commons/2/21/VK.com-logo.svg"},
+    { text: "1.VK", link: "https://vk.com/oo0010090"},
     { text: "2.Telegram", link: "https://t.me/ProkopenkoSR"},
     { text: "3.GitHub", link: "https://github.com/serptid"},
     { text: "4.Discord", link: "https://discord.com/users/533201254143229962"},
@@ -23,59 +71,16 @@ const menuItems = [
     { text: "6.Spotify", link: "https://open.spotify.com/user/31xappvpl5vfcxp5mw6oslo4zpha?si=47467846e8cc4f60"}
 ];
 
+const first = document.getElementById("first");
+const menu = document.getElementById("menu");
+const next_1 = document.getElementById("next_1");
 
-// Функция для побуквенного вывода текста
-function typeText(text, element, callback, speed = 10) {
-    let i = 0;
-    const interval = setInterval(() => {
-        if (i < text.length) {
-            element.textContent += text[i];
-            i++;
-        } else {
-            clearInterval(interval);
-            if (callback) callback(); // Вызываем коллбэк после завершения
-        }
-    }, speed);
-}
+const Write = new TextWriter(first, 20);
+const WriteMenu = new MenuRenderer(menu, menuItems, 50);
 
-// Функция для побуквенного добавления меню
-function typeMenuItem(menuItem, callback) {
-    let i = 0;
-
-    // Создаём элемент ссылки
-    const menuLink = document.createElement("a");
-    menuLink.href = menuItem.link;
-    menuLink.target = "_blank";
-    menuLink.className = "menu-item";
-
-    outputArea.appendChild(menuLink);
-
-    const interval = setInterval(() => {
-        if (i < menuItem.text.length) {
-            menuLink.textContent += menuItem.text[i];
-            i++;
-        } else {
-            clearInterval(interval);
-            if (callback) callback();
-        }
-    }, 50);
-}
-
-// Функция для запуска вывода меню
-function startMenu(callback) {
-    const renderMenu = (index) => {
-        if (index < menuItems.length) {
-            typeMenuItem(menuItems[index], () => renderMenu(index + 1));
-        } else if (callback) {
-            callback(); // Вызываем коллбэк после завершения меню
-        }
-    };
-    renderMenu(0);
-}
-
-// Последовательный запуск функций
-typeText(introText, output, () => {
-    startMenu(() => {
-        typeText(bottomText, outputBottom); // Выводим bottomText после меню
+Write.typeText(introText, () => {
+    WriteMenu.renderMenu(() => {
+        const WriteBottom = new TextWriter(next_1, 10);
+        WriteBottom.typeText(bottomText);
     });
 });

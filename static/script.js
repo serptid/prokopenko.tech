27297b20b784eb2aa8337168660bb2ac929d4f39
Сергей Прокопенко <psi200523@gmail.com>
2025-01-07 15:31:40 +0900

    ор 	modified:   static/index.html 	modified:   static/script.js

--- static/script.js
+++ static/script.js
@@ -1,22 +1,35 @@
 class TextWriter {
-    constructor(element, speed = 50) {
-        this.element = element;
+    constructor(outputArea, texts, speed = 50) {
+        this.outputArea = outputArea;
+        this.texts = texts;
         this.speed = speed;
     }
 
-    typeText(text, callback) {
-        let i = 0;
-        const interval = setInterval(() => {
-            if (i < text.length) {
-                this.element.textContent += text[i];
-                i++;
-            } else {
-                clearInterval(interval);
-                if (callback) callback();
-            }
-        }, this.speed);
+    async typeText(text) {
+        return new Promise((resolve) => {
+            let i = 0;
+            const interval = setInterval(() => {
+                if (i < text.length) {
+                    this.outputArea.innerHTML += text[i]; // Используем innerHTML для контроля разметки
+                    i++;
+                } else {
+                    clearInterval(interval);
+                    resolve();
+                }
+            }, this.speed);
+        });
+    }
+
+    async renderTexts() {
+        for (const text of this.texts) {
+            const span = document.createElement("span"); // Оборачиваем текст в <span>
+            this.outputArea.appendChild(span);
+            await this.typeText(text);
+        }
     }
 }
+
+
 
 class MenuRenderer {
     constructor(outputArea, menuItems, speed = 50) {
@@ -25,62 +38,78 @@ class MenuRenderer {
         this.speed = speed;
     }
 
-    typeMenuItem(menuItem, callback) {
-        let i = 0;
+    async typeMenuItem(menuItem) {
+        return new Promise((resolve) => {
+            let i = 0;
+            const menuLink = document.createElement("a");
+            menuLink.href = menuItem.link;
+            menuLink.target = "_blank";
+            menuLink.className = "menu-item";
+            this.outputArea.appendChild(menuLink);
 
-        // Создаём элемент ссылки
-        const menuLink = document.createElement("a");
-        menuLink.href = menuItem.link;
-        menuLink.target = "_blank";
-        menuLink.className = "menu-item";
-
-        this.outputArea.appendChild(menuLink);
-
-        const interval = setInterval(() => {
-            if (i < menuItem.text.length) {
-                menuLink.textContent += menuItem.text[i];
-                i++;
-            } else {
-                clearInterval(interval);
-                if (callback) callback();
-            }
-        }, this.speed);
+            const interval = setInterval(() => {
+                if (i < menuItem.text.length) {
+                    menuLink.textContent += menuItem.text[i];
+                    i++;
+                } else {
+                    clearInterval(interval);
+                    menuLink.textContent += " "; // Добавляем пробел вместо переноса строки
+                    resolve();
+                }
+            }, this.speed);
+        });
     }
 
-    renderMenu(callback) {
-        const renderMenuRecursively = (index) => {
-            if (index < this.menuItems.length) {
-                this.typeMenuItem(this.menuItems[index], () => renderMenuRecursively(index + 1));
-            } else if (callback) {
-                callback();
-            }
-        };
-        renderMenuRecursively(0);
+    async renderMenu() {
+        for (const menuItem of this.menuItems) {
+            await this.typeMenuItem(menuItem);
+        }
     }
 }
 
-// Использование классов
-const introText = "Добро пожаловать на мой первый сайт.\n\nМеня зовут Прокопенко Сергей Игоревич. Я студент Дальневосточного Федерального Университета.";
-const bottomText = "Все права защищены© ";
+
+// Тексты
+const introTexts = [
+    "Добро пожаловать на мой первый сайт.\n",
+    "Меня зовут Прокопенко Сергей Игоревич. Я студент Дальневосточного Федерального Университета."
+];
+const bottomTexts = ["Все права защищены©"];
+// Меню
 const menuItems = [
-    { text: "1.VK", link: "https://vk.com/oo0010090"},
-    { text: "2.Telegram", link: "https://t.me/ProkopenkoSR"},
-    { text: "3.GitHub", link: "https://github.com/serptid"},
-    { text: "4.Discord", link: "https://discord.com/users/533201254143229962"},
-    { text: "5.Steam", link: "https://steamcommunity.com/id/SERP_TID"},
-    { text: "6.Spotify", link: "https://open.spotify.com/user/31xappvpl5vfcxp5mw6oslo4zpha?si=47467846e8cc4f60"}
+    { text: "1.VK", link: "https://vk.com/oo0010090" },
+    { text: "2.Telegram", link: "https://t.me/ProkopenkoSR" },
+    { text: "3.GitHub", link: "https://github.com/serptid" },
+    { text: "4.Discord", link: "https://discord.com/users/533201254143229962" },
+    { text: "5.Steam", link: "https://steamcommunity.com/id/SERP_TID" },
+    { text: "6.Spotify", link: "https://open.spotify.com/user/31xappvpl5vfcxp5mw6oslo4zpha?si=47467846e8cc4f60" }
+];
+const bottom_menu = [
+    { text: "1.Книги", link: "https://vk.com/oo0010090" },
+    { text: "2.Музыка", link: "https://vk.com/oo0010090" },
+    { text: "3.Фильмы", link: "https://vk.com/oo0010090" },
+
 ];
 
+
+// Элементы DOM
 const first = document.getElementById("first");
-const menu = document.getElementById("menu");
-const next_1 = document.getElementById("next_1");
+const menu_1 = document.getElementById("menu_1");
+const second = document.getElementById("second");
+const menu_2 = document.getElementById("menu_2");
 
-const Write = new TextWriter(first, 20);
-const WriteMenu = new MenuRenderer(menu, menuItems, 50);
+// Экземпляры классов
+const IntroWriter = new TextWriter(first, introTexts, 20);
+const Menu1Renderer = new MenuRenderer(menu_1, menuItems, 20);
+const BottomWriter = new TextWriter(second, bottomTexts, 20);
+const Menu2Renderer = new MenuRenderer(menu_2, bottom_menu, 20);
 
-Write.typeText(introText, () => {
-    WriteMenu.renderMenu(() => {
-        const WriteBottom = new TextWriter(next_1, 10);
-        WriteBottom.typeText(bottomText);
-    });
-});
+// Асинхронный порядок выполнения
+async function execute() {
+    await IntroWriter.renderTexts();     // Вывод introTexts в first
+    await Menu1Renderer.renderMenu();   // Вывод menuItems в menu_1
+    await BottomWriter.renderTexts();   // Вывод bottomTexts в second
+    await Menu2Renderer.renderMenu();   // Вывод bottom_menu в menu_2
+    console.log("Все блоки текста и меню завершены.");
+}
+
+execute();
